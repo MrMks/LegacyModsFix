5e02896893106312289897245976c8f95b5dd11e
MrMks <i0991958@163.com>
2022-08-30 21:42:18 +0800

    some adjusts and update to 0.0.2-b4

--- src/main/java/com/github/mrmks/mc/lmf/fmp/MixinControlKeyModifier.java
+++ src/main/java/com/github/mrmks/mc/lmf/fmp/MixinControlKeyModifier.java
@@ -15,13 +15,17 @@ import scala.collection.mutable.WeakHashMap;
 @Mixin(ControlKeyModifer$.class)
 public class MixinControlKeyModifier {
 
-    @Shadow(remap = false) @Final
-    private Map<EntityPlayer, Boolean> map;
+//    @Shadow(remap = false) @Final
+//    private Map<EntityPlayer, Boolean> map;
 
-    @Redirect(method = "<init>", at = @At(value = "FIELD", opcode = Opcodes.PUTFIELD, target = "Lcodechicken/multipart/ControlKeyModifer$;map:Lscala/collection/mutable/Map;", remap = false), remap = false)
-    private void setMapValue(@Coerce Object owner, @Coerce Object f) {
-        Map<EntityPlayer, Boolean> m = new WeakHashMap<>();
-        map = m.withDefaultValue(false);
+//    @Redirect(method = "<init>", at = @At(value = "FIELD", opcode = Opcodes.PUTFIELD, target = "Lcodechicken/multipart/ControlKeyModifer$;map:Lscala/collection/mutable/Map;", remap = false), remap = false)
+//    private void setMapValue(@Coerce Object owner, @Coerce Object f) {
+//        Map<EntityPlayer, Boolean> m = new WeakHashMap<>();
+//        map = m.withDefaultValue(false);
+//    }
+
+    @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lscala/collection/mutable/Map;withDefaultValue(Ljava/lang/Object;)Lscala/collection/mutable/Map;"), remap = false)
+    private Map proxyWithDefaultValue(@Coerce Object callee, @Coerce Object arg) {
+        return new WeakHashMap<EntityPlayer, Boolean>().withDefaultValue(Boolean.FALSE);
     }
-
 }
--- src/main/java/com/github/mrmks/mc/lmf/nyasamalib/MixinTrain.java
+++ src/main/java/com/github/mrmks/mc/lmf/nyasamalib/MixinTrain.java
@@ -3,11 +3,8 @@ package com.github.mrmks.mc.lmf.nyasamalib;
 import net.minecraft.world.World;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.thewdj.linkage.core.Train;
 
 import java.lang.ref.WeakReference;
@@ -15,12 +12,10 @@ import java.lang.ref.WeakReference;
 @Mixin(Train.class)
 public class MixinTrain {
     private WeakReference<World> worldRef;
-    @Shadow(remap = false) private World world;
 
-    @Inject(method = "<init>", at = @At("RETURN"), remap = false)
-    private void onConstruct(CallbackInfo ci) {
+    @Redirect(method = "<init>", at = @At(value = "FIELD", opcode = Opcodes.PUTFIELD, target = "Lorg/thewdj/linkage/core/Train;world:Lnet/minecraft/world/World;"), remap = false)
+    private void setWorldValueInit(Train train, World world) {
         this.worldRef = new WeakReference<>(world);
-        this.world = null;
     }
 
     @Redirect(method = {"lambda$get$0", "lambda$getTrainRaw$2", "access$302"}, at = @At(value = "FIELD", opcode = Opcodes.PUTFIELD, target = "Lorg/thewdj/linkage/core/Train;world:Lnet/minecraft/world/World;"), remap = false)
