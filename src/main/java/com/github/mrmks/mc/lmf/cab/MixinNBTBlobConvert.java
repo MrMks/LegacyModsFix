ed1e673acc1b72eb2008bd9b2cf02a14d24afa6a
MrMks <i0991958@163.com>
2023-02-01 11:04:57 +0800

    * 0.0.6-b16
    b16: use class visitor to prevent early class load.

--- src/main/java/com/github/mrmks/mc/lmf/cab/MixinNBTBlobConvert.java
+++ src/main/java/com/github/mrmks/mc/lmf/cab/MixinNBTBlobConvert.java
@@ -1,19 +1,22 @@
 package com.github.mrmks.mc.lmf.cab;
 
 import mod.chiselsandbits.chiseledblock.NBTBlobConverter;
+import mod.chiselsandbits.chiseledblock.TileEntityBlockChiseled;
 import mod.chiselsandbits.chiseledblock.data.VoxelBlobStateReference;
 import net.minecraft.nbt.NBTTagCompound;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(NBTBlobConverter.class)
 public class MixinNBTBlobConvert {
 
+    @Shadow(remap = false)
+    TileEntityBlockChiseled tile;
     private static final String NBT_VERSIONED_FORMAT = "Xf";
 
     @Inject(
@@ -27,17 +30,36 @@ public class MixinNBTBlobConvert {
 
     @Redirect(
             method = "readChisleData",
-            slice = @Slice(
-                    from = @At(value = "INVOKE", target = "Lnet/minecraft/nbt/NBTTagCompound;getInteger(Ljava/lang/String;)I")
-            ),
-            at = @At(value = "INVOKE", target = "Lmod/chiselsandbits/chiseledblock/data/VoxelBlobStateReference;getFormat()I", ordinal = 0, remap = false)
+            at = @At(value = "INVOKE", target = "Lmod/chiselsandbits/chiseledblock/data/VoxelBlobStateReference;getFormat()I", ordinal = 1),
+            remap = false
     )
     public int redirectGetFormat(VoxelBlobStateReference reference, NBTTagCompound compound) {
         if (compound.hasKey(NBT_VERSIONED_FORMAT, 3)) {
             int ret = compound.getInteger(NBT_VERSIONED_FORMAT);
             ((VoxelBlobStateInstanceAccessor) (Object) reference.getInstance()).setFormat(ret);
             return ret;
-        } else return reference.getFormat();
+        } else {
+            tile.markDirty();
+            return reference.getFormat();
+        }
+    }
+
+    @Redirect(
+            method = "readChisleData",
+            at = @At(value = "INVOKE", target = "Lmod/chiselsandbits/chiseledblock/data/VoxelBlobStateReference;getFormat()I", ordinal = 0),
+            remap = false
+    )
+    public int redirectGetFormat0(VoxelBlobStateReference reference) {
+        return reference.getFormat();
+    }
+
+    @Redirect(
+            method = "readChisleData",
+            at = @At(value = "INVOKE", target = "Lmod/chiselsandbits/chiseledblock/data/VoxelBlobStateReference;getFormat()I", ordinal = 2),
+            remap = false
+    )
+    public int redirectGetFormat2(VoxelBlobStateReference reference) {
+        return reference.getFormat();
     }
 
 }
